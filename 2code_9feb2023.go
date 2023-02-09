5c7ebec42c8433c00ae08d95c7af1a23869ab481
“jubayer_ahmed” <jasamrat@gmail.com>
2023-02-09 17:34:39 +0600

    initial slice commit

--- 2code_9feb2023.go
+++ 2code_9feb2023.go
@@ -5,25 +5,6 @@ import "fmt"
 func main() {
 
 
-	var students [3]string
-	students[0] = "jubayer"
-	students[1] = "rofik"
-	students[2] = "minhaz"
-
-	fmt.Println(students)
-	fmt.Println(len(students))
-	
-	fmt.Println(students[0])
-	fmt.Println(students[1])
-	fmt.Println(students[2])
-	
-	fmt.Println()
-	fmt.Println()
-	
-// short hand way
-	
-	students2 := [...]string{"samad", "anik", "rafi", "himel", "opu"}
-	fmt.Println(students2, len(students2))
 
 
 
