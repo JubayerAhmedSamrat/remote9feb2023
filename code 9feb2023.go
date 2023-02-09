3a60c8ef3b41cafc7a516fd70e5e828e8f2d54d0
“jubayer_ahmed” <jasamrat@gmail.com>
2023-02-09 15:20:57 +0600

    9feb 3rd try

---
+++ code 9feb2023.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+
+func main() {
+
+	var name string
+	var age int
+	var result float32
+	var chr rune
+	
+	chr = 'p'
+
+	var isFound bool
+
+	fmt.Println(name)
+	fmt.Println(age)
+	fmt.Println(result)
+	fmt.Println(chr)
+	fmt.Println(isFound)
+
+	fmt.Println("%c", chr)
+
+	fmt.Println(true && true)
+	fmt.Println(true && false)
+	fmt.Println(true || true)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+
+
+
+
+}
