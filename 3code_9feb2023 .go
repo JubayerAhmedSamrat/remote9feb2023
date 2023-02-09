a6788bc81a8f72a3b0d5e39ede899fd0ab0a8b57
“jubayer_ahmed” <jasamrat@gmail.com>
2023-02-09 20:05:18 +0600

    2nd comit on data type

--- 3code_9feb2023 .go
+++ 3code_9feb2023 .go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 
-type Book struct{
+/*type Book struct{
 
 		Title string
 		Author string
@@ -12,17 +12,36 @@ type Book struct{
 		Pages int
 
 	}
+*/
 
 func main() {
 	
 
+/*
 	var b1 Book
-	b1.Title = "An Introduction to programming in GO"
-	b1.Author = "CALEB DOXY"
-	b1.ISBN = "67489-9889-7987"
-	b1.Price = 10.50
-	b1.Pages = 165
+	b1.Title = "An Introduction to programming in GO",
+	b1.Author = "CALEB DOXY",
+	b1.ISBN = "67489-9889-7987",
+	b1.Price = 10.50,
+	b1.Pages = 165,
 
+*/
+	
+
+//annonymus data type
+	b1 := struct{
+		Title string
+		Author string
+		ISBN  string
+		Price float32
+		Pages int
+}{
+	Title : "An Introduction to programming in GO"
+	Author : "CALEB DOXY"
+	ISBN : "67489-9889-7987"
+	Price : 10.50
+	Pages : 165
+}
 
 	fmt.Println(b1)
 	fmt.Println(b1.Title)
@@ -30,7 +49,6 @@ func main() {
 	fmt.Println(b1.ISBN)
 	fmt.Println(b1.Price)
 	fmt.Println(b1.Pages)
-	
 
 	
 
