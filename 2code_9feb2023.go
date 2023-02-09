cc316d64ce3fb5950a3779209b8695257ae8fd00
“jubayer_ahmed” <jasamrat@gmail.com>
2023-02-09 18:03:49 +0600

    slice done

--- 2code_9feb2023.go
+++ 2code_9feb2023.go
@@ -1,11 +1,35 @@
 package main
 
 import "fmt"
+import "reflect"
 
 func main() {
 
 
+	var students [3]string
 
+	students[0] = "jubayer"
+	students[1] = "sagor"
+	students[2] = "toufiq"
+
+	x := students[0:3]
+	fmt.Println(x)
+
+	y := make([]string, 3)
+	fmt.Println(y)
+	
+	var fruits []string
+	fruits = append(fruits, "apple", "banana", "mango")
+	fmt.Println(fruits, len(fruits))
+	fmt.Println("%T \n", fruits)
+	fmt.Println("%T", students)
+
+	a := reflect.TypeOf(students).Kind().String()
+	fmt.Println(a)
+	
+	b := reflect.TypeOf(fruits).Kind().String()
+	fmt.Println(b)
+	
 
 
 
